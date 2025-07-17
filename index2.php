8893edb726d8f0747c4cdba5139127c846a88909
lumbani <munthalilu75@gmail.com>
2025-07-17 15:56:54 +0200

    this is day 4

---
+++ index2.php
@@ -0,0 +1,13 @@
+<?php
+class Student {
+    public $name;
+    public $age;
+    public $gender;
+    
+    public function _ construct($name, $age, $gender);
+
+$this->name = $name;
+$this->age = $age;
+$this->gender = $gender;
+
+    
